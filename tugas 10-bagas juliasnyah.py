3e71e3a5dff03eaa24564d9a8a26847a69951392
bgass2 <153707769+bgass2@users.noreply.github.com>
2023-12-18 00:43:00 +0800

    tugas 10-bagas juliasnyah.py

---
+++ tugas 10-bagas juliasnyah.py
@@ -0,0 +1,8 @@
+Nim = 231031124
+
+bilangan = int(input("Masukkan bilangan: "))
+               
+if bilangan % 2 == 0:
+    print("Bilangan", bilangan, "adalah bilangan genap.")
+else:
+    print("Bilangan", bilangan, "adalah bilangan ganjil.")
